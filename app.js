2788992077ada4aded324a29abff2ee08b9b6fb1
j.majurel <j.majurel@bpp-tech.com>
2018-01-10 19:01:17 +0000

    added styling

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var campgrounds = [
+    { name: "Praloup", image: "http://www.photosforclass.com/download/15989950903" },
+    { name: "Canigou", image: "http://www.photosforclass.com/download/1430198323" },
+    { name: "Saint Victoire", image: "http://www.photosforclass.com/download/3662521481" },
+    { name: "Praloup", image: "http://www.photosforclass.com/download/15989950903" },
+    { name: "Canigou", image: "http://www.photosforclass.com/download/1430198323" },
+    { name: "Saint Victoire", image: "http://www.photosforclass.com/download/3662521481" },
+    { name: "Praloup", image: "http://www.photosforclass.com/download/15989950903" },
+    { name: "Canigou", image: "http://www.photosforclass.com/download/1430198323" },
+    { name: "Saint Victoire", image: "http://www.photosforclass.com/download/3662521481" }
+];
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('newCamp');
+});
+
 app.get('/campgrounds', (req, res) => {
-  var campgrounds = [
-    { name: "Praloup", image: "http://www.photosforclass.com/download/15989950903" },
-    { name: "Canigou", image: "http://www.photosforclass.com/download/1430198323" },
-    { name: "Saint Victoire", image: "http://www.photosforclass.com/download/3662521481" },
-  ];
   res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', (req, res) => {
+  var newCampground = {name: req.body.name, image: req.body.image}; 
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
