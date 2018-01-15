bfa85a704b55d7933b2e167508c897b3421a2307
j.majurel <j.majurel@bpp-tech.com>
2018-01-15 23:34:11 +0000

    added show page

--- app.js
+++ app.js
@@ -1,52 +1,65 @@
 var express    = require('express'),
+    app        = express(),
     bodyParser = require('body-parser'),
     mongoose   = require('mongoose');
 
-var app = express();
-
-mongoose.connect('mongodb://localhost/yelpCamp', { useMongoClient: true });
-
-var campgroundSchema = mongoose.Schema(
-  {
-    name: String,
-    image: String
-  }
-);
-var Campgrounds = mongoose.model('campground', campgroundSchema);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect('mongodb://localhost/yelpCamp');
+
+var campgroundSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    desc: String
+});
+
+var Campground = mongoose.model('campground', campgroundSchema);
+
 app.get('/', (req, res) => {
   res.render('landing');
+});
+
+
+app.get('/campgrounds', function(req, res) {
+  Campground.find(function(err, data) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render('index', {campgrounds: data});
+    }
+  });
+});
+
+app.post('/campgrounds', function(req, res) {
+
+  var newCampground = new Campground({
+    name: req.body.name,
+    image: req.body.image,
+    desc: req.body.description
+  });
+
+  newCampground.save(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
 app.get('/campgrounds/new', (req, res) => {
   res.render('newCamp');
 });
 
-app.get('/campgrounds', (req, res) => {
-  Campgrounds.find(function(err, data) {
+app.get('/campgrounds/:id', (req, res) => {
+  Campground.findById(req.params.id, function(err, foundItem){
     if(err) {
       console.log(err);
     } else {
-      res.render('campgrounds', {campgrounds: data});
-    }
-  });
-});
-
-app.post('/campgrounds', (req, res) => {
-  var newCampground = new Campgrounds({
-    name: req.body.name,
-    image: req.body.image
-  });
-
-  newCampground.save(function(err, data) {
-    if(err) {
-      console.log(err);
-    } else {
-      res.redirect('/campgrounds');
+      res.render('show', {campground: foundItem});
     }
   });
 });
