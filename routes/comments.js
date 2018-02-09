76aaaa9c6ce8cc454fd83ecc68134522f34c8334
j.majurel <j.majurel@bpp-tech.com>
2018-02-09 19:48:21 +0000

    added comments update/destroy routes and authentification

--- routes/comments.js
+++ routes/comments.js
@@ -40,6 +40,42 @@ router.get('/new', isLoggedIn, (req, res) => {
   });
 });
 
+//EDIT
+router.get('/:comment_id/edit', isLoggedIn, checkOwnership, function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, com) {
+    if(err){
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.render('comments/edit', {campground_id: req.params.id, comment: com});
+    }
+  });
+});
+
+//UPDATE
+router.put('/:comment_id', isLoggedIn, checkOwnership, function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err){
+    if(err){
+      console.log(err);
+      res.redirect("/campgrounds/" +req.params.id);
+    } else {
+      res.redirect("/campgrounds/" +req.params.id);
+    }
+  });
+});
+
+//DESTROY
+router.delete("/:comment_id", isLoggedIn, checkOwnership, function(req, res) {
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err) {
+      console.log(err);
+      res.redirect("/campgrounds/" + req.params.id);
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 //middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
@@ -48,5 +84,21 @@ function isLoggedIn(req, res, next){
     res.redirect("/login");
   }
 };
+
+//Middleware for Authorization 
+function checkOwnership(req, res, next) {
+  Comment.findById(req.params.comment_id, function(err, foundItem){
+    if(err){
+      console.log(err);
+      res.redirect("back");
+    } else {
+      if(foundItem.author.id.equals(req.user._id)){
+	next();
+      } else {
+	res.redirect("back");
+      }
+    }
+  });
+}
 
 module.exports = router;
